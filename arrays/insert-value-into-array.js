3fb04a16d521e8bd644c129be9f9dbcc1fec0da8
Edem <50662754+newfaceee@users.noreply.github.com>
2020-05-18 08:57:30 +0300

    Удаляет закоментированный код

--- arrays/insert-value-into-array.js
+++ arrays/insert-value-into-array.js
@@ -1,8 +1,3 @@
-// const insert = (arr, index, value) => {
-//   return [...arr.slice(0, index), value, ...arr.slice(index)];
-// }
-// console.log(insert([1,2,3], 2, 42));
-
 Array.prototype.insert = function (index, value) {
   for (let i = 0; i < value.length; i++) {
     return;
@@ -10,6 +5,5 @@ Array.prototype.insert = function (index, value) {
 
   return [...this.slice(0, index), value, ...this.slice(index)];
 }
-const arr = [0];
 
 console.log(arr.insert(0,0).insert(10, 2).insert(0, 10));
